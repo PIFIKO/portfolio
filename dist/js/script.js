c793231ee9e142b30f6b6c2c6b1a0a00109c3e43
Ivan <pifiko@ukr.net>
2022-05-22 00:29:50 +0300

    start

---
+++ dist/js/script.js
@@ -0,0 +1,62 @@
+window.addEventListener('DOMContentLoaded', () => {
+const humburger = document.querySelector('.hambureger'),
+      menu = document.querySelector('.menu'),
+      closeElem= document.querySelector('.menu__close'),
+      social = document.querySelectorAll('[data-modal]');
+    
+
+      
+
+//humburger
+humburger.addEventListener('click', ()=>{
+    menu.classList.add('active');
+    humburger.classList.add('active');
+  
+});
+closeElem.addEventListener('click', ()=>{
+    menu.classList.remove('active');
+    humburger.classList.remove('active');
+});
+//svg
+document.querySelectorAll('img.img-svg').forEach(img => {
+    var imgId = img.id;
+    var imgClass = img.className;
+    var imgURL = img.src;
+    var imgFill = img.getAttribute('data-fill');
+    
+    fetch(imgURL).then(r => r.text()).then(text => {
+        var parser = new DOMParser();
+        var xmlDoc = parser.parseFromString(text, "text/xml");
+        var svg = xmlDoc.getElementsByTagName('svg')[0];
+        
+        if (typeof imgId !== 'undefined') {
+            svg.setAttribute('id', imgId);
+        }
+        
+        if (typeof imgClass !== 'undefined') {
+            svg.setAttribute('class', imgClass);
+        }        
+        
+        if (typeof imgFill !== 'undefined') {
+            svg.setAttribute('fill', imgFill);
+        }
+        
+        img.parentNode.replaceChild(svg, img);
+        
+    }).catch(console.error);
+    
+});
+//link social
+window.addEventListener('scroll',() =>{
+let scrollpos = window.scrollY;
+const scrollChange = 400;
+social.forEach(e =>{
+    if (scrollpos >= scrollChange){
+        e.classList.add("active");
+    }else{
+            e.classList.remove("active");
+        }
+    
+});
+});
+});
